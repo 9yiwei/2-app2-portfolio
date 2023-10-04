5930d7641832eaa3b4e0c312b0c34507bf892c71
Howrad <9yiwei@gmail.com>
2023-10-04 11:55:10 +0800

    Create col, img, and description

---
+++ main.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.jpg", use_column_width="auto")
+
+with col2:
+    st.title("Howard Chou")
+    content = """
+    My last job was as a software project assistant, so I understand the program development process and due to work 
+    requirements, I will need to conduct website testing and record bugs, prepare relevant test documents, etc.
+    
+    Also because of the requirements of my last job, I also need to simplify the requests of customers or superiors 
+    so that the people receiving the message can understand the problem in a simple and easy-to-understand manner.
+    
+    Because I understand my own shortcomings in programming, I have continued to self-study Python-related courses 
+    on the website so far.
+    """
+    st.info(content)
