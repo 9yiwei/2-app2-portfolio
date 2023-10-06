5e45b6bfd778638585e494ecfdb06520844b8de4
Howrad <9yiwei@gmail.com>
2023-10-06 09:08:33 +0800

    Day23: Add Contact_us pages and sendmail function

---
+++ pages/Contact_Us.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from send_mail import send_mail
+
+st.header("Contact Me!")
+
+with st.form(key="email_form"):
+    user_email = st.text_input("Your email address")
+    raw_message = st.text_area("Your message")
+    message = f"""\
+Subject: Job-Things from {user_email}
+
+{raw_message}
+From: {user_email}
+"""
+    button = st.form_submit_button("Submit")
+    if button:
+        send_mail(message)
+        st.info("Your email was sent successfully!")
